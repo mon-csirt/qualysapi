7618056cceff0e688368033871fc236708f09756
Riley V <fmtlinpqma@p.monash.edu>
2025-08-22 15:44:47 +1000

    tweak Host to take data from GAV API, create Tag

--- qualysapi/api_objects.py
+++ qualysapi/api_objects.py
@@ -4,25 +4,28 @@ from lxml import objectify
 
 
 class Host:
-    def __init__(self, dns, id, ip, last_scan, netbios, os, tracking_method):
+    def __init__(self, dns, id, host_type,created,modified):
         self.dns = str(dns)
         self.id = int(id)
-        self.ip = str(ip)
-        try:
-            last_scan = str(last_scan).replace("T", " ").replace("Z", "").split(" ")
-            date = last_scan[0].split("-")
-            time = last_scan[1].split(":")
-            self.last_scan = datetime.datetime(
-                int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), int(time[2])
-            )
-        except IndexError:
-            self.last_scan = "never"
-        self.netbios = str(netbios)
-        self.os = str(os)
-        self.tracking_method = str(tracking_method)
+        self.host_type = str(host_type)
+        self.created = str(created)
+        self.modified = str(modified)
+        # self.ip = str(ip)
+        # try:
+        #     last_scan = str(last_scan).replace("T", " ").replace("Z", "").split(" ")
+        #     date = last_scan[0].split("-")
+        #     time = last_scan[1].split(":")
+        #     self.last_scan = datetime.datetime(
+        #         int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), int(time[2])
+        #     )
+        # except IndexError:
+        #     self.last_scan = "never"
+        # self.netbios = str(netbios)
+        # self.os = str(os)
+        # self.tracking_method = str(tracking_method)
 
     def __repr__(self):
-        return f"ip: {self.ip}, qualys_id: {self.id}, dns: {self.dns}"
+        return f"qualys_id: {self.id}, dns: {self.dns}"
 
 
 class VirtualHost:
@@ -203,3 +206,14 @@ class Scanner:
         self.running_slices_count = running_slices_count
         self.running_scan_count = running_scan_count
         self.status = status
+
+class Tag:
+    def __init__(self, name, id, colour, created, modified):
+        self.name = str(name)
+        self.id = int(id)
+        self.colour = str(colour)
+        self.created = str(created)
+        self.modified = str(modified)
+        
+    def __repr__(self):
+        return f"qualys_id: {self.id}, name: {self.name}"
